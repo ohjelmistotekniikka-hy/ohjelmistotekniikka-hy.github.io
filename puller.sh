0836f2d5d52f10dc9c9822e495f4aa810c66e6ca
Riku Rauhala <riku@rauhala.io>
2023-04-05 14:54:57 +0300

    Update puller.sh

--- puller.sh
+++ puller.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-# store the current dir
+# Instructions:
+# 1. Clone all repositories into a directory on your computer
+#   - Use ssh, not https
+#   - Use the students' real names for the subdirectories
+#   - $ git clone git@github.com:username/repository.git FirstnameLastname
+# 2. Make the script executable
+#   - $ chmod +x pull.sh
+# 3. Run the script
+#   - $ ./pull.sh
 
-CUR_DIR=\$(pwd)
-
-# Let the person running the script know what's going on.
+CURRENT_DIRECTORY=$(pwd)
 
 echo "Pulling in latest changes for all repositories..."
 
-# Find all git repositories and update it to the master latest revision
-
 for i in $(find . -name ".git" | cut -c 3-); do
-    echo "";
-    echo "$i"; # We have to go to the .git parent directory to call the pull command
-cd "$i";
-cd ..; # finally pull
-git checkout master;
-git reset --hard HEAD
-git pull; # lets get back to the CUR_DIR
-cd $CUR_DIR
+    echo ""
+    echo "$(dirname "$i")"
+    cd "$(dirname "$i")"
+    if git show-ref --verify --quiet refs/heads/main; then
+        git checkout main
+    else
+        git checkout master
+    fi
+    git reset --hard HEAD
+    git pull
+    cd "$CURRENT_DIRECTORY"
 done
+
 echo "Complete!"
